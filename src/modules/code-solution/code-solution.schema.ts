a4ee055d94b70f8c27de3b4a0795699d83827654
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2025-10-25 22:49:58 +0530

    update: Code solution schema for judge0 and new routes support

--- src/modules/code-solution/code-solution.schema.ts
+++ src/modules/code-solution/code-solution.schema.ts
@@ -1,21 +1,7 @@
 import { z } from 'zod';
-import { CodeSolutionStatus } from '../../generated/prisma';
 
 export const CodeSolutionCreateSchema = z.object({
   code: z.string().min(1, 'Code is required'),
-});
-
-export const CodeSolutionStatusUpdateSchema = z.object({
-  status: z.enum([CodeSolutionStatus.GRADED, CodeSolutionStatus.SUBMITTED], {
-    errorMap: () => ({ message: 'Invalid status' }),
-  }),
-});
-
-export const CodeSolutionScoreUpdateSchema = z.object({
-  score: z
-    .number()
-    .min(0, 'Score must be a non-negative number')
-    .max(100, 'Score must be at most 100'),
 });
 
 export const CodeSolutionAssesmentIdParamSchema = z.object({
@@ -29,8 +15,32 @@ export const CodeSolutionIdParamSchema = z.object({
   id: z.string().cuid('Invalid code assessment ID format'),
 });
 
+export const CodeSolutionIdAndTestCaseIdParamSchema = z.object({
+  courseId: z.string().cuid('Invalid course ID format'),
+  codeAssessmentId: z.string().cuid('Invalid code assessment ID format'),
+  codeSolutionId: z.string().cuid('Invalid code solution ID format'),
+  testCaseId: z.string().cuid('Invalid test case ID format'),
+  judge0AuthToken: z.string(),
+});
+
+export const Judge0SubmissionSchema = z.object({
+  stdout: z.string().nullable(),
+  time: z.string().nullable(),
+  memory: z.number().nullable(),
+  stderr: z.string().nullable(),
+  token: z.string(),
+  compile_output: z.string().nullable(),
+  message: z.string().nullable(),
+  status: z.object({
+    id: z.number(),
+    description: z.string(),
+  }),
+});
+
 export type CodeSolutionCreateType = z.infer<typeof CodeSolutionCreateSchema>;
-export type CodeSolutionStatusUpdateType = z.infer<typeof CodeSolutionStatusUpdateSchema>;
-export type CodeSolutionScoreUpdateType = z.infer<typeof CodeSolutionScoreUpdateSchema>;
 export type CodeSolutionAssesmentIdParamType = z.infer<typeof CodeSolutionAssesmentIdParamSchema>;
 export type CodeSolutionIdParamType = z.infer<typeof CodeSolutionIdParamSchema>;
+export type Judge0SubmissionType = z.infer<typeof Judge0SubmissionSchema>;
+export type CodeSolutionIdAndTestCaseIdParamType = z.infer<
+  typeof CodeSolutionIdAndTestCaseIdParamSchema
+>;
