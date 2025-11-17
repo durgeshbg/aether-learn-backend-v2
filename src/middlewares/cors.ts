e23abb4d64e2bc397546923ffcaff3ad5497c115
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2025-11-17 18:58:47 +0530

    update: enhance CORS middleware to allow specific origins and improve error handling

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -2,10 +2,24 @@ import cors from 'cors';
 import type { Application } from 'express';
 
 export const useCors = (app: Application) => {
+  const origins = process.env.CORS_ORIGIN
+    ? process.env.CORS_ORIGIN.split(',').map((origin) => origin.trim())
+    : [];
+
   app.use(
     cors({
-      origin: process.env.CORS_ORIGIN || '*', // Allow all origins by default, can be configured via environment variable
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
+      origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (origins.indexOf(origin) === -1) {
+          const msg =
+            `The CORS policy for this site does not ` +
+            `allow access from the specified Origin: ${origin}.`;
+          return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+      },
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
       credentials: true, // Allow credentials (cookies, authorization headers, etc.)
       optionsSuccessStatus: 204, // For legacy browser support
