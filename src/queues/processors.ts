56a0c9426e637608080cd3618f3f78ec13ea9bbf
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2026-04-05 11:40:17 +0530

    Revert "refactor: streamline JUDGE0 submission request in CodeExecutionProcessor by removing redundant try-catch block"
    This reverts commit 91a5d557b434ee746c56ddea84ee0d12d714bf57.

--- src/queues/processors.ts
+++ src/queues/processors.ts
@@ -8,32 +8,32 @@ export default class CodeExecutionProcessor {
     const queue = new QueueService().getQueue(Queues.CODE_EXECUTION);
     if (!queue) return;
 
-    const { data } = await axios.post(
-      process.env.JUDGE0_URL + '/submissions',
-      {
-        source_code: job.data.sourceCode,
-        language_id: job.data.languageId,
-        stdin: job.data.stdin,
-        expected_output: job.data.expectedOutput,
-        callback_url: `${process.env.JUDGE0_CB_BASE}/api/v1/courses/${job.data.courseId}/code-assessments/${job.data.codeAssessmentId}/code-solutions/${job.data.codeSolutionId}/test-cases/${job.data.testCaseId}/judge0-submission/${process.env.JUDGE0_AUTH_TOKEN}`,
-      },
-      {
-        params: {
-          base64_encoded: 'false',
+    try {
+      const { data } = await axios.post(
+        process.env.JUDGE0_URL + '/submissions',
+        {
+          source_code: job.data.sourceCode,
+          language_id: job.data.languageId,
+          stdin: job.data.stdin,
+          expected_output: job.data.expectedOutput,
+          callback_url: `${process.env.JUDGE0_CB_BASE}/api/v1/courses/${job.data.courseId}/code-assessments/${job.data.codeAssessmentId}/code-solutions/${job.data.codeSolutionId}/test-cases/${job.data.testCaseId}/judge0-submission/${process.env.JUDGE0_AUTH_TOKEN}`,
         },
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Auth-Token': process.env.JUDGE0_AUTH_TOKEN || '',
+        {
+          params: {
+            base64_encoded: 'false',
+          },
+          headers: {
+            'Content-Type': 'application/json',
+            'X-Auth-Token': process.env.JUDGE0_AUTH_TOKEN || '',
+          },
         },
-      },
-    );
+      );
 
-    const testingCallbackUrl = `${process.env.JUDGE0_CB_BASE}/api/v1/courses/${job.data.courseId}/code-assessments/${job.data.codeAssessmentId}/code-solutions/${job.data.codeSolutionId}/test-cases/${job.data.testCaseId}/judge0-submission/${process.env.JUDGE0_AUTH_TOKEN}`;
-    const { data: testingCallbackData } = await axios.get(testingCallbackUrl);
-    console.log(`Testing callback data: ${JSON.stringify(testingCallbackData)}`);
+      console.log(`Process job with id ${job.id} from the ${Queues.CODE_EXECUTION} queue`);
 
-    console.log(`Process job with id ${job.id} from the ${Queues.CODE_EXECUTION} queue`);
-
-    return data;
+      return data;
+    } catch (error) {
+      console.error(`Error running test: ${error}`);
+    }
   }
 }
