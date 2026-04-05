91a5d557b434ee746c56ddea84ee0d12d714bf57
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2026-04-05 09:55:05 +0530

    refactor: streamline JUDGE0 submission request in CodeExecutionProcessor by removing redundant try-catch block

--- src/queues/processors.ts
+++ src/queues/processors.ts
@@ -8,32 +8,28 @@ export default class CodeExecutionProcessor {
     const queue = new QueueService().getQueue(Queues.CODE_EXECUTION);
     if (!queue) return;
 
-    try {
-      const { data } = await axios.post(
-        process.env.JUDGE0_URL + '/submissions',
-        {
-          source_code: job.data.sourceCode,
-          language_id: job.data.languageId,
-          stdin: job.data.stdin,
-          expected_output: job.data.expectedOutput,
-          callback_url: `${process.env.JUDGE0_CB_BASE}/api/v1/courses/${job.data.courseId}/code-assessments/${job.data.codeAssessmentId}/code-solutions/${job.data.codeSolutionId}/test-cases/${job.data.testCaseId}/judge0-submission/${process.env.JUDGE0_AUTH_TOKEN}`,
+    const { data } = await axios.post(
+      process.env.JUDGE0_URL + '/submissions',
+      {
+        source_code: job.data.sourceCode,
+        language_id: job.data.languageId,
+        stdin: job.data.stdin,
+        expected_output: job.data.expectedOutput,
+        callback_url: `${process.env.JUDGE0_CB_BASE}/api/v1/courses/${job.data.courseId}/code-assessments/${job.data.codeAssessmentId}/code-solutions/${job.data.codeSolutionId}/test-cases/${job.data.testCaseId}/judge0-submission/${process.env.JUDGE0_AUTH_TOKEN}`,
+      },
+      {
+        params: {
+          base64_encoded: 'false',
         },
-        {
-          params: {
-            base64_encoded: 'false',
-          },
-          headers: {
-            'Content-Type': 'application/json',
-            'X-Auth-Token': process.env.JUDGE0_AUTH_TOKEN || '',
-          },
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Auth-Token': process.env.JUDGE0_AUTH_TOKEN || '',
         },
-      );
+      },
+    );
 
-      console.log(`Process job with id ${job.id} from the ${Queues.CODE_EXECUTION} queue`);
+    console.log(`Process job with id ${job.id} from the ${Queues.CODE_EXECUTION} queue`);
 
-      return data;
-    } catch (error) {
-      console.error(`Error running test: ${error}`);
-    }
+    return data;
   }
 }
